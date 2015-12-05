26996e7f672a346bf1fb83cbe1f744cf30cfb8aa
Zindel, Christian (FRA-MRM) <Christian.Zindel@mrm-mccann.de>
2015-12-05 22:43:02 +0100

    basic HTML setup

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -14,17 +10,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+
+app.use('/', require('./routes/index'));
+
+app.post('/rest_api/blogposts', function () {});
+app.get('/rest_api/blogposts', function () {});
+app.get('/rest_api/blogposts/:id', function () {});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
