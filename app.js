7134cb2b18f2d8d4777cb592e33dc6273def2fb7
Zindel, Christian (FRA-MRM) <Christian.Zindel@mrm-mccann.de>
2015-12-07 16:22:42 +0100

    rest service for new post and list of posts

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var uuid = require('node-uuid');
+var fs = require('fs');
+var path = require('path');
 
 
 var app = express();
@@ -15,14 +18,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
-
+app.use('/views', express.static(path.join(__dirname, 'app/views')));
 
 
 app.use('/', require('./routes/index'));
 
-app.post('/rest_api/blogposts', function () {});
-app.get('/rest_api/blogposts', function () {});
-app.get('/rest_api/blogposts/:id', function () {});
+var blogPostData = path.resolve('blogPostData.json');
+
+app.post('/rest_api/blogposts', function (req, res, next) {
+
+    req.body.created_timestamp = new Date().toISOString();
+    req.body.id = uuid.v4();
+
+    fs.readFile(blogPostData, function (err, data) {
+
+        if (err) {
+            res.status(500).send('fs read error');
+        }
+        else{
+
+            var jsonData = JSON.parse(data);
+            jsonData.push(req.body);
+
+            fs.writeFile(blogPostData, JSON.stringify(jsonData, null, '    '), function (err) {
+                if (err) {
+                    res.status(500).send('fs write error');
+                }
+                else{
+                    res.send(req.body)
+                }
+            });
+        }
+    });
+});
+app.get('/rest_api/blogposts', function (req, res, next) {
+    res.sendFile(blogPostData);
+});
+app.get('/rest_api/blogposts/:id', function (req, res, next) {
+
+});
 
 
 // catch 404 and forward to error handler
